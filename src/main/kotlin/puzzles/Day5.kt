21a55f0aad8a1ccc7486d0bed4144f0424702883
Wai kon Tse <waikon@waikontse.info>
2021-12-05 15:41:03 +0100

    Initial Day5

---
+++ src/main/kotlin/puzzles/Day5.kt
@@ -0,0 +1,19 @@
+package puzzles
+
+class Day5: Puzzle {
+    override fun solvePart1(): Int {
+        TODO()
+    }
+
+    override fun solvePart2(): Int {
+        TODO()
+    }
+
+    override fun solveDemoPart1(): Int {
+        TODO()
+    }
+
+    override fun solveDemoPart2(): Int {
+        TODO()
+    }
+}
---
+++ src/test/kotlin/puzzles/Day5Test.kt
@@ -0,0 +1,25 @@
+package puzzles
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day5Test {
+    private val puzzle = Day5()
+
+    @Test
+    fun solvePart1() {
+    }
+
+    @Test
+    fun solvePart2() {
+    }
+
+    @Test
+    fun solveDemoPart1() {
+        assertThat(puzzle.solveDemoPart1()).isEqualTo(5)
+    }
+
+    @Test
+    fun solveDemoPart2() {
+    }
+}
