df4689d06a86cccd1345970a017c1707d89e5bb9
Wai kon Tse <wai.kon.tse@ximedes.com>
2021-12-08 23:38:10 +0100

    Add day 8 solution.

---
+++ src/main/kotlin/puzzles/Day8.kt
@@ -0,0 +1,87 @@
+package puzzles
+
+import util.ReaderUtil
+
+class Day8: Puzzle(8) {
+    val inputDemo = ReaderUtil.readResourceAsStrings("input8demo.txt")
+    val input = ReaderUtil.readResourceAsStrings("input8.txt")
+
+    val distanceToNumer: Map<Int, List<Pair<Int, Int>>> = mapOf(
+        1 to listOf(4 to 2, 3 to 3, 4 to 5, 5 to 6, 4 to 9, 4 to 0),
+        4 to listOf(3 to 2, 2 to 3, 2 to 5, 3 to 6, 2 to 9, 3 to 0),
+        7 to listOf(3 to 2, 2 to 3, 3 to 5, 4 to 6, 3 to 9, 3 to 0),
+        8 to listOf(2 to 2, 2 to 3, 2 to 5, 1 to 6, 1 to 9, 1 to 0)
+    )
+
+    override fun solveDemoPart1(): String {
+        return solve(inputDemo).toString()
+    }
+
+    override fun solveDemoPart2(): String {
+        return inputDemo.map { solve3(it) }
+            .map { it.toInt() }
+            .sum().toString()
+    }
+
+    private fun solve3(input: String): String {
+        val fullListRight = input.split(" | ")[1].split(" ")
+        val fullListLeft = input.split(" | ")[0].split(" ")
+        val uniqueLeft = fullListLeft.let { filterUnique(it) }
+
+        return fullListRight.map {determineNumber(it, uniqueLeft)}.fold("") {acc, i -> acc+i }
+    }
+
+    private fun filterUnique(list: List<String>): List<String> {
+        return list.filter { it.length == 2 || it.length == 4 || it.length == 3 || it.length == 7 }
+    }
+
+    private fun determineNumber(unknown: String, unique: List<String>): Int {
+        return if (stringToNumber(unknown) != -1) {
+            stringToNumber(unknown)
+        } else determineUnknown(unknown, unique)
+    }
+
+    private fun determineUnknown(unknown: String, unique: List<String>): Int {
+        val listCandidates = unique.map { stringToNumber(it) to getDistance(it, unknown) }
+            .map { distanceToNumer[it.first]?.filter { p -> p.first == it.second } }
+            .map { it!!.map { pair -> pair.second } }
+            .flatten().groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
+
+        return listCandidates!!
+    }
+
+    override fun solvePart1(): String {
+        return solve(input).toString()
+    }
+
+    override fun solvePart2(): String {
+        return input.map { solve3(it) }.sumOf { it.toInt() }.toString()
+    }
+
+    private fun solve(input: List<String>): Int {
+        return input.map { it.split(" | ") }
+            .map { it[1] }
+            .flatMap { it.split(" ") }
+            .map { it.length }.count { it == 2 || it == 4 || it == 3 || it == 7 }
+    }
+
+    private fun stringToNumber(length: String): Int {
+        return when(length.length) {
+            2 -> 1
+            4 -> 4
+            3 -> 7
+            7 -> 8
+            else -> -1
+        }
+    }
+
+    private fun getDistance(unique: String, to: String): Int {
+        val toSet = to.toSet()
+        val uniqueSet = unique.toSet()
+        val distanceD =
+            if (uniqueSet.size < toSet.size) toSet.minus(uniqueSet).size
+            else uniqueSet.minus(toSet).size
+
+        return distanceD
+    }
+}
---
+++ src/test/kotlin/puzzles/Day8Test.kt
@@ -0,0 +1,30 @@
+package puzzles
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day8Test {
+    val puzzle = Day8()
+
+    @Test
+    fun solveDemoPart1() {
+        assertThat(puzzle.solveDemoPart1()).isEqualTo("26")
+    }
+
+    @Test
+    fun solveDemoPart2() {
+        assertThat(puzzle.solveDemoPart2()).isEqualTo("61229")
+    }
+
+    @Test
+    fun solvePart1() {
+        assertThat(puzzle.solvePart1()).isEqualTo("321")
+    }
+
+    @Test
+    fun solvePart2() {
+        assertThat(puzzle.solvePart2()).isEqualTo("1028926")
+    }
+}
