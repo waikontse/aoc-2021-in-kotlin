7e900afa4f155a4d2755559d00be6216ce51ac5c
Wai kon Tse <waikon@waikontse.info>
2021-12-09 08:39:34 +0100

    Add Day9 initial implementation

---
+++ src/main/kotlin/puzzles/Day9.kt
@@ -0,0 +1,19 @@
+package puzzles
+
+class Day9: Puzzle(9) {
+    override fun solveDemoPart1(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun solveDemoPart2(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePart1(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePart2(): String {
+        TODO("Not yet implemented")
+    }
+}
