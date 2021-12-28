bb7c15bae943dfb55ca3702c28026cd290354c7d
Wai kon Tse <waikon@waikontse.info>
2021-12-28 11:41:36 +0100

    Add day 16 partial solution

--- src/main/kotlin/puzzles/week3/Day16.kt
+++ src/main/kotlin/puzzles/week3/Day16.kt
@@ -2,17 +2,20 @@ package puzzles.week3
 
 import puzzles.Puzzle
 
+data class Packet(val version: Int, val typeId: Int, val lengthType: Int = -1, val literal: Int, val subPackets: List<Packet>)
+data class ParseInfo(val input: String, val currentPos: Int) {
+    fun movePos(increment: Int): ParseInfo = this.copy(currentPos = this.currentPos + increment)
+    fun charAtPos(): Char = input[currentPos]
+    fun substring(length: Int): String = input.substring(currentPos until currentPos + length)
+    fun charsLeft(): Int = input.length - currentPos
+}
+
 class Day16: Puzzle(16) {
     override fun solveDemoPart1(): String {
         val binaryString = convertToBinary(inputDemo.first())
-        val version = parseVersion(binaryString)
-        val type = parseType(binaryString.drop(3))
-
-        println ("$version $type")
-
-        if(type == 4) {
-            println(parseLiteral(binaryString.drop(6)))
-        }
+        //val parseInfo = ParseInfo(binaryString, 0)
+        val packet = parsePackets(binaryString)
+        println("$packet")
 
         return convertToBinary(inputDemo.first())
     }
@@ -46,25 +49,83 @@ class Day16: Puzzle(16) {
         return convertToBinary(input.toCharArray().toList(), "", mapHexToBin)
     }
 
-    private fun parseVersion(input: String): Int {
-        return input.substring((0..2)).toInt(2)
-    }
-
-    private fun parseType(input: String): Int {
-        return input.substring((0..2)).toInt(2)
-    }
-
-    private fun parseLiteral(input: String): Pair<Int, Int> {
-        tailrec fun parseLiteral(input: String, decoded: String, sectionCount: Int): Pair<Int, Int> {
-            if (input.startsWith('0')) {
-                val section = input.substring(0..4).drop(1)
-                return Pair(decoded.plus(section).toInt(2), sectionCount.inc()*5)
+    private fun parsePackets(input: String): List<Packet> {
+        tailrec fun parsePackets(parseInfo: ParseInfo, parsedPackets: List<Packet>): List<Packet> {
+            if (parseInfo.charsLeft() < 8) {
+                return parsedPackets
             }
 
-            val section = input.substring(0..4).drop(1)
-            return parseLiteral(input.drop(5), decoded.plus(section), sectionCount.inc())
+            val newlyParsedPacket = parsePacket(parseInfo)
+            return parsePackets(newlyParsedPacket.second, parsedPackets.plus(newlyParsedPacket.first))
         }
 
-        return parseLiteral(input, "", 0)
+        return parsePackets(ParseInfo(input, 0), listOf())
+    }
+
+    private fun parsePacket(parseInfo: ParseInfo): Pair<Packet, ParseInfo> {
+        println ("Starting parse packet $parseInfo")
+        // parseVersion
+        val version = parseVersion(parseInfo)
+        // parseType
+        val typeId = parseType(version.second)
+        // parse literal / parse operator
+        val literal: Pair<Int, ParseInfo>
+        val lengthType: Pair<Int, ParseInfo>
+        val subPackets: List<Packet>
+        if (typeId.first == 4) {
+            literal = parseLiteral(typeId.second)
+            lengthType = -1 to parseInfo
+            subPackets = listOf()
+        } else {
+            lengthType = parseLengthType(typeId.second)
+            literal = parseLengthLiteral(lengthType.second, lengthType.first)
+            subPackets = listOf()
+        }
+
+        return Packet(version.first, typeId.first, lengthType.first, literal.first, subPackets) to literal.second
+    }
+
+    private fun parseVersion(parseInfo: ParseInfo): Pair<Int, ParseInfo> {
+        return parseInfo.substring(3).toInt(2) to parseInfo.movePos(3)
+    }
+
+    private fun parseType(parseInfo: ParseInfo): Pair<Int, ParseInfo> {
+        return parseInfo.substring(3).toInt(2) to parseInfo.movePos(3)
+    }
+
+    private fun parseLiteral(parseInfo: ParseInfo): Pair<Int, ParseInfo> {
+        tailrec fun parseLiteral(parseInfo: ParseInfo, decoded: String): Pair<Int, ParseInfo> {
+            println ("ParseLiteral 2  $parseInfo")
+            if (parseInfo.charAtPos() == '0') {
+                val section = parseInfo.substring(5).drop(1)
+                return decoded.plus(section).toInt(2) to parseInfo.movePos(5)
+            }
+
+            val section = parseInfo.substring(5).drop(1)
+            return parseLiteral(parseInfo.movePos(5), decoded.plus(section))
+        }
+
+        println ("ParseLiteral 1 $parseInfo")
+
+        return parseLiteral(parseInfo, "")
+    }
+
+    private fun parseLengthType(parseInfo: ParseInfo): Pair<Int, ParseInfo> {
+        return parseInfo.substring(1).toInt() to parseInfo.movePos(1)
+    }
+
+    private fun parseLengthLiteral(parseInfo: ParseInfo, lengthType: Int): Pair<Int, ParseInfo> {
+        //println("parse length literal $parseInfo ${parseInfo.substring(15)}")
+        return when(lengthType) {
+            0 -> parseInfo.substring(15).toInt(2) to parseInfo.movePos(15)
+            1 -> parseInfo.substring(11).toInt(2) to parseInfo.movePos(2)
+            else -> throw UnsupportedOperationException("Wrong length type: $lengthType")
+        }
+    }
+
+    private fun parseOperator(parseInfo: ParseInfo, lengthType: Int, lengthLiteral: Int): Pair<List<Packet>, ParseInfo> {
+        when (lengthType) {
+
+        }
     }
 }
